642e8e61e45f5e92ce5261f4fd71353689c605a1
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-18 01:19:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import time
 if os.name == "nt":
   for i in range(50):
     os.system("start")
 else:
   for i in range(10):
     os.system("gnome-terminal -- hollywood")
+    time.sleep(10)
