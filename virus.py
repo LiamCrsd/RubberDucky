e3f77ac664cd482744f72f946417d70631978d22
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-25 00:47:44 +0100

    Update virus.py

--- virus.py
+++ virus.py
@@ -2,7 +2,7 @@ import os
 import time
 if os.name == "nt":
   for i in range(50):
-    os.system("start python script2/script3.py")
+    os.system("start python script2/sousvirus.py")
     time.sleep(1)
 else:
   for i in range(20):
