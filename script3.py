03b8a8513fdeb4be3c4f73a5ca9c56aa3439b5f2
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-24 16:21:17 +0100

    Update script3.py

--- script3.py
+++ script3.py
@@ -1,2 +1,8 @@
-while True:
+fic = open("data.txt","r")
+var = bool(fic.readline())
+fic.close()
+while var:
+  fic = open("data.txt","r")
+  var = bool(fic.readline())
+  fic.close()
   print("Hack en cours")
