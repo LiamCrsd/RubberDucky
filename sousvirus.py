614e93bad2b7cfd7ef4ff98f6f553dc46675cb5d
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-25 00:52:17 +0100

    Update sousvirus.py

--- sousvirus.py
+++ sousvirus.py
@@ -1,9 +1,14 @@
+import time
 fic = open("script2/data.txt","r")
-var = bool(fic.readline())
+var = int((fic.readline())[14:])
 fic.close()
+count = 0
 while var:
+  count +=1
+  x = int((count/50)*40)
   fic = open("script2/data.txt","r")
-  var = bool(fic.readline())
+  var = int((fic.readline())[14:])
   fic.close()
-  print("Hack en cours",var)
+  print("Hack en cours " + "[" + "#"*x + "."*(40-x) + "]" )
+  time.sleep(1)
 print("Echec du Hack")
