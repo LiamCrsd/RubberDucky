3bb389e24699d028b1edad23f2920bcecfdfa68a
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2023-02-24 16:08:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import time
 if os.name == "nt":
   for i in range(50):
-    os.system("start")
+    os.system("start tree")
+    time.sleep(1)
 else:
   for i in range(20):
     os.system("gnome-terminal -- hollywood")
